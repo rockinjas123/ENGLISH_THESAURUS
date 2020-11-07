5267891a1181cf1a88c0f2474619fcf684d0fadd
rockinjas123 <ankjas123gmail.com>
2020-11-07 16:23:51 +0530

    version 2

--- V_1_DICT.py
+++ V_1_DICT.py
@@ -7,16 +7,20 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+   elif w.title() in data:
+        return data[w.title()]
+   elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+   elif len(get_close_matches(w, data.keys())) > 0:
+       yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
         else:
             return "We didn't understand your entry."
-    else:
-        return "The word doesn't exist. Please double check it."
+   else:
+       return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 output = translate(word)
